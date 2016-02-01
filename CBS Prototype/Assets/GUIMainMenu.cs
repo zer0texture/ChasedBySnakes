1d67d844cc647e325abae2f7af61718e8208933c
up659154 <up659154@myport.ac.uk>
2016-02-01 14:21:41 +0000

    added volume slider in ingame menu (+ tidy menu code)

--- CBS Prototype/Assets/GUIMainMenu.cs
+++ CBS Prototype/Assets/GUIMainMenu.cs
@@ -3,9 +3,6 @@ using System.Collections;
 
 public class GUIMainMenu : MonoBehaviour, GameSaveManager.IGameSaver
 {
-    //private bool pauseEnabled;
-
-
     public bool isMainMenu;
     public bool isMainMenuOptions;
     public bool isMainMenuSliders;
@@ -15,18 +12,6 @@ public class GUIMainMenu : MonoBehaviour, GameSaveManager.IGameSaver
     public static float barMaxSliderValue = 100.0f;
     public static float snakeMaxSliderValue = 50.0f;
     public static float otherMaxSliderValue = 10.0f;
-
-    //public static float healthCurrentSliderValue;
-    //public static float oilCurrentSliderValue;
-    //public static float snakeCurrentSliderValue; 
-    //public static float lanternCurrentSliderValue;
-    //public static float trailCurrentSliderValue;
-
-    //public static float healthTotalSliderValue = barMaxSliderValue - healthCurrentSliderValue;
-    //public static float oilTotalSliderValue = barMaxSliderValue - oilCurrentSliderValue;
-    //public static float snakeTotalSliderValue = snakeMaxSliderValue - snakeCurrentSliderValue;
-    //public static float lanternTotalSliderValue = otherMaxSliderValue - lanternCurrentSliderValue;
-    //public static float trailTotalSliderValue = otherMaxSliderValue - trailCurrentSliderValue;
 
     static public float currentVolume = 1.0F;
 
@@ -47,132 +32,67 @@ public class GUIMainMenu : MonoBehaviour, GameSaveManager.IGameSaver
 
     void Start()
     {
-        //pauseEnabled = false;
         isMainMenu = true;
         isMainMenuOptions = false;
         isMainMenuSliders = false;
         isMainMenuOptionsAudio = false;
-        Time.timeScale = 1;  //??
-        AudioListener.volume = currentVolume; //??
-        //AudioListener.pause = false;
-        //Cursor.visible = false; // Hide Cursor
+        Time.timeScale = 1;
+        AudioListener.volume = currentVolume;
 
         Load();
         Save();
         
     }
 
-    void Update()
-    {
-
-        //check if pause button (escape key) is pressed
-        /*if (Input.GetKeyDown("escape"))
-        {
-            isInGameMainMenu = true;
-            isInGameOptions = false;
-            isInGameOptionsAudio = false;
-            //check if game is already paused        
-            if (pauseEnabled == true)
-            {
-                //unpause the game
-                pauseEnabled = false;
-                Time.timeScale = 1; //??
-                AudioListener.volume = currentVolume; //??
-                AudioListener.pause = false;
-                Cursor.visible = false;
-            }
-
-            //else if game isn't paused, then pause it
-            else if (pauseEnabled == false)
-            {
-                pauseEnabled = true;
-                AudioListener.volume = currentVolume; //??
-                AudioListener.pause = true;
-                Time.timeScale = 0; //??
-                Cursor.visible = true;
-            }
-        }*/
-    }
-
     void OnGUI()
     {
 
-        //GUI.DrawTexture(new Rect(50, 50, 120, 120), aTexture, ScaleMode.StretchToFill, true, 10.0F);
-
-       /*GUI.skin.box.font = pauseMenuFont; // ^see above at 'pauseMenuFont' definition - currently set to NULL
-        GUI.skin.button.font = pauseMenuFont; // ^see above at 'pauseMenuFont' definition - currently set to NULL
-        GUI.backgroundColor = Color.white; // colour for boxes
-        GUI.skin.box.border.left = 1;
-        GUI.skin.box.border.right = 1;
-        GUI.skin.box.border.top = 1;*/
-
-
-        //GUI.contentColor = Color.green; // colour for text in boxes
-        //GUI.color = Color.green; // colour for everything in GUI
-
-        //if (pauseEnabled == true)
-       // {
             if (isMainMenu == true)
-            {
-                //Make a background box -- (in format of: (width initial position, height initial position, width scale, height scale), text to display)                         
+            {                     
                 GUI.Box(new Rect(0, 0, Screen.width, Screen.height), "Main Menu");
 
-                //Make Resume button
                 if (GUI.Button(new Rect(Screen.width / 2 - 100, Screen.height / 2 - 100, 250, 40), "New Game"))
                 {
-                    Application.LoadLevel(2);
-
-                //AudioListener.volume = 1;                 
-
+                    Application.LoadLevel(2);                
                 }
 
-                //Make load button
                 if (GUI.Button(new Rect(Screen.width / 2 - 100, Screen.height / 2 - 60, 250, 40), "Load Last Save"))
                 {
-                    //INSERT LAST SAVE POINT
-                    /*
                     GameSaveManager.m_Instance.StartLoading();
                     GameSaveManager.SceneState save = new GameSaveManager.SceneState();
                     save.Load();
                     Application.LoadLevel(save.m_SceneNo);
-                    */
-                }
+            }
 
-                //Make save button
                 if (GUI.Button(new Rect(Screen.width / 2 - 100, Screen.height / 2 - 20, 250, 40), "Sliders"))
                 {
                     isMainMenuSliders = true;
                     isMainMenu = false;
                 }
 
-                //Make Options button
                 if (GUI.Button(new Rect(Screen.width / 2 - 100, Screen.height / 2 + 20, 250, 40), "Options"))
                 {
                     isMainMenuOptions = true;
                     isMainMenu = false;
                 }
 
-                //Make Main Menu button
                 if (GUI.Button(new Rect(Screen.width / 2 - 100, Screen.height / 2 + 70, 250, 40), "Return to Main Menu"))
                 {
                     Application.LoadLevel(0);
                 }
 
-                //Make quit game button
                 if (GUI.Button(new Rect(Screen.width / 2 - 100, Screen.height / 2 + 110, 250, 40), "Quit to Windows"))
                 {
-                    Application.Quit(); // quits application
+                    Application.Quit();
                 }
             }
 
                 if(isMainMenuSliders == true)
-                    {
-                        //Make a background box -- (in format of: (width initial position, height initial position, width scale, height scale), text to display) ..or maybe not..                         
+                    {               
                         GUI.Box(new Rect(0, 0, Screen.width, Screen.height), "SLIDERS");
 
                         sliderValue = GUI.HorizontalSlider(new Rect((Screen.width / 2) - 50,  50, 150, 30), UISlider.GetSliderValue(UISlider.SliderType.HEALTH), 0.0F, barMaxSliderValue);
                         UISlider.SetSliderValue(UISlider.SliderType.HEALTH, sliderValue);
-                       // sliderValue = healthCurrentSliderValue;
 
                                 GUI.Label(new Rect((Screen.width / 2) - 45, 30, 300, 20), "Amount of Player Health");
                                 GUI.Label(new Rect((Screen.width / 2) - 60, 50, 100, 20), "0");
@@ -180,7 +100,6 @@ public class GUIMainMenu : MonoBehaviour, GameSaveManager.IGameSaver
 
                         sliderValue = GUI.HorizontalSlider(new Rect((Screen.width / 2) - 50,  100, 150, 30), UISlider.GetSliderValue(UISlider.SliderType.OIL_MAX), 0.0F, barMaxSliderValue);
                         UISlider.SetSliderValue(UISlider.SliderType.OIL_MAX, sliderValue);
-                       // sliderValue = oilCurrentSliderValue;
 
                                 GUI.Label(new Rect((Screen.width / 2) - 35, 80, 300, 20), "Amount of Player Oil");
                                 GUI.Label(new Rect((Screen.width / 2) - 60, 100, 100, 20), "0");
@@ -188,7 +107,6 @@ public class GUIMainMenu : MonoBehaviour, GameSaveManager.IGameSaver
 
                         sliderValue = GUI.HorizontalSlider(new Rect((Screen.width / 2) - 50,  150, 150, 30), UISlider.GetSliderValue(UISlider.SliderType.NUM_OF_SNAKES), 0.0F, snakeMaxSliderValue);
                         UISlider.SetSliderValue(UISlider.SliderType.NUM_OF_SNAKES, sliderValue);
-                       // sliderValue = snakeCurrentSliderValue;
 
                                 GUI.Label(new Rect((Screen.width / 2) - 30, 130, 300, 20), "Number of Snakes");
                                 GUI.Label(new Rect((Screen.width / 2) - 60, 150, 100, 20), "0");
@@ -196,7 +114,6 @@ public class GUIMainMenu : MonoBehaviour, GameSaveManager.IGameSaver
 
                         sliderValue = GUI.HorizontalSlider(new Rect((Screen.width / 2) - 50, 200, 150, 30), UISlider.GetSliderValue(UISlider.SliderType.LANTERN_SIZE), 0.0F, otherMaxSliderValue);
                         UISlider.SetSliderValue(UISlider.SliderType.LANTERN_SIZE, sliderValue);
-                       // sliderValue = lanternCurrentSliderValue;
 
                                 GUI.Label(new Rect((Screen.width / 2) - 30, 180, 300, 20), "Player Lantern Size");
                                 GUI.Label(new Rect((Screen.width / 2) - 60, 200, 100, 20), "0");
@@ -204,13 +121,12 @@ public class GUIMainMenu : MonoBehaviour, GameSaveManager.IGameSaver
 
                         sliderValue = GUI.HorizontalSlider(new Rect((Screen.width / 2) - 50, 250, 150, 30), UISlider.GetSliderValue(UISlider.SliderType.LENGTH_OF_TRAIL), 0.0F, otherMaxSliderValue);
                         UISlider.SetSliderValue(UISlider.SliderType.LENGTH_OF_TRAIL, sliderValue);
-                       // sliderValue = trailCurrentSliderValue;
 
                                 GUI.Label(new Rect((Screen.width / 2) - 35, 230, 300, 20), "Length of Player Trail");
                                 GUI.Label(new Rect((Screen.width / 2) - 60, 250, 100, 20), "0");
                                 GUI.Label(new Rect((Screen.width / 2) + 105, 250, 100, 20), "10");
 
-            if (GUI.Button(new Rect(Screen.width / 2 - 100, 300, 250, 50), "Back"))
+                         if (GUI.Button(new Rect(Screen.width / 2 - 100, 300, 250, 50), "Back"))
                         {
                             isMainMenuSliders = false;
                             isMainMenu = true;
@@ -218,21 +134,18 @@ public class GUIMainMenu : MonoBehaviour, GameSaveManager.IGameSaver
 
                     }
 
-                if (isMainMenuOptions == true)
-                    {
-                        //Make a background box -- (in format of: (width initial position, height initial position, width scale, height scale), text to display) ..or maybe not..                         
+                    if (isMainMenuOptions == true)
+                    {                       
                         GUI.Box(new Rect(0, 0, Screen.width, Screen.height), "OPTIONS");
 
                     if (GUI.Button(new Rect(Screen.width / 2 - 100, Screen.height / 2 - 100, 250, 50), "Audio Settings"))
                         {
-                        //INSERT SETTINGS HERE
                             isMainMenuOptionsAudio = true;
                             isMainMenuOptions = false;
-                            // AudioListener.volume = 0;
                         }
                         if (GUI.Button(new Rect(Screen.width / 2 - 100, Screen.height / 2 - 50, 250, 50), "Display Settings"))
                         {
-                            //INSERT SETTINGS HERE
+
                         }
 
                         if (GUI.Button(new Rect(Screen.width / 2 - 100, Screen.height / 2 + 100, 250, 50), "Back"))
